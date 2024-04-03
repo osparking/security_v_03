ec56a5483c4146968bd373cca808c432f2bb765e
osparking <jbpark03@gmail.com>
2024-04-04 06:23:49 +0900

    authen & authori S boot 3.0

---
+++ src/main/java/space/bum/security03/SecurityV03Application.java
@@ -0,0 +1,13 @@
+package space.bum.security03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityV03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityV03Application.class, args);
+	}
+
+}
---
+++ src/test/java/space/bum/security03/SecurityV03ApplicationTests.java
@@ -0,0 +1,13 @@
+package space.bum.security03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityV03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
